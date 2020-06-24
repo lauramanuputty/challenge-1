8c53ce1e444d859a83367dc4dcd49ca702f7a90d
lauramanuputty <lauramanuputty@users.noreply.github.com>
2020-06-24 20:52:27 +0200

    images

--- script/main.js
+++ script/main.js
@@ -1,23 +1,2 @@
- google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-	      ['Water', 'Stock'],
-          ['Water',     11],
-          ['',      2],
-        ]);
+var myChart = document.getElementById('myChart').getContext('2d');
 
-        var options = {
-          title: 'My Daily Activities',
-          pieHole: 0.4,
-		  slices: {
-			0: { color: '#80C5DE' },
-            1: { color: 'transparent' },
-		
-			  
-		  }
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
